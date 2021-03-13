d15b4ab90b20a15109ac037a53f482bb9346ad3e
valentina comerci <valentinacomerci3@gmail.com>
2021-03-13 05:26:07 -0800

    React Router Parameter

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -42,6 +42,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
@@ -49,5 +50,14 @@ class Main extends Component {
         );
     };
 }
+
+const CampsiteWithId = ({match}) => {
+    return (
+        <CampsiteInfo 
+            campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+            comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+        />
+    );
+};    
 
 export default Main;
