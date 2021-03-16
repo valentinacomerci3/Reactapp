5cefb95d7cd709a8ed11514adef6ce3162a97ddb
valentina comerci <valentinacomerci3@gmail.com>
2021-03-16 01:29:56 -0700

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent'
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutComponent'
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
@@ -43,6 +44,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route exact path='/contactus' component={Contact} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
